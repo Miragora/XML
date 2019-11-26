a2c4a35a26a233166d40fa0f97eae2448c65bd22
stud <stud@RUDN-422-8>
2019-11-26 11:15:00 +0300

    Добавьте файлы проекта.

---
+++ XML/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML
+{
+    public class StudentGroup
+    {
+        //свойства - название
+        public string Name { get; set;}
+        public List<Student> Students { get; set; } = new List<Student>();
+
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public List<int> Ratings { get; set; } = new List<int>();
+    }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Group = new StudentGroup();
+            Group.Name = "BirthdayOfKaterina";
+            for (int i = 1; i <= 10; i++)
+            {
+                var student = new Student();
+                student.Name = "Студент" + i;
+                student.SecondName = "Фамилия " + i;
+                student.Age = 18 + i;
+                Group.Students.Add(student);
+            }
+            var serializer = new XmlSerializer(typeof(StudentGroup));    
+            using (var file = File.Create("students.xml"))
+            {
+                serializer.Serialize(file, Group);
+            }
+        
+
+
+            Console.ReadLine();
+        }
+    }
+}
